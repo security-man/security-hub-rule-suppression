efbc1270dace8c02284477b58bc545d2a55033e0
security-man <zac.laker@crowncommercial.gov.uk>
2023-08-08 16:01:28 +0100

    Modified script to use aws-organizations api to poll for up-to-date account ids

--- aws-security-hub-rule-disable.py
+++ aws-security-hub-rule-disable.py
@@ -1,54 +1,58 @@
-import argparse
 import boto3
 import logging
-import time
-import os
 import csv
 from botocore.client import ClientError
 from botocore.exceptions import ParamValidationError
 
-# setup account input details and define aws profiles
-input_switch_statement = input('Choose input type: 1 = comma-separated list of profiles, 2 = path to aws .config file: ')
-if input_switch_statement == '1':
-    profile_string = input('Specify AWS Profile List: ')
-    profile_list = profile_string.split(",")
-elif input_switch_statement == '2':
-    profile_string = input('Specify .aws config file path: ')
-    profile_list = []
+# establish client with root account (default profile)
+session = boto3.Session(profile_name="default")
+client = session.client('sts')
+org = session.client('organizations')
 
-    # Using readlines()
-    config_file = open(profile_string, 'r')
-    file_lines = config_file.readlines()
+# paginate through all org accounts
+paginator = org.get_paginator('list_accounts')
+page_iterator = paginator.paginate()
 
-    # Strips the newline character
-    for line in file_lines:
-        line_contents = line.split()
-        if len(line_contents) > 0:
-            if line_contents[0] == '[profile':
-                profile_name = line_contents[1].split(']')
-                profile_list.append(profile_name[0])
-else:
-    raise Exception("Sorry, no numbers below zero")
+# get accounts ids for active accounts
+account_ids = []
+print("Getting full list of accounts from AWS Organizations ...")
+for page in page_iterator:
+    for account in page['Accounts']:
+        if account['Status'] == 'ACTIVE':
+            account_ids += {account['Id']}
+
+print("Account list populated!")
+print("")
 
 file_contents = []
 
 # opens control-suppression-list csv
+print("Reading list of controls to suppress ...")
 with open('control-suppression-list.csv') as f:
     reader = csv.reader(f)
     for row in reader:
         file_contents.append(row)
 
+# initiates sessions / clients per aws profile (account combined with security audit role)
+sessions = []
+securityhub_clients = []
+for account_id in account_ids:
+    profile = account_id + "-RO"
+    session = boto3.Session(profile_name=profile)
+    sessions.append(session)
+    securityhub_client = session.client("securityhub", region_name="eu-west-2")
+    securityhub_clients.append(securityhub_client)
+
+# loops through list of controls to suppress from csv, and loops through each individual profile to make the changes to
+# securityhub api is rate-limited to 1 request per second, per account
 for contents in file_contents:
-    for profile in profile_list:
-        session = boto3.Session(profile_name=profile)
-        securityhub_client = session.client("securityhub", region_name="eu-west-2")
-        element = contents[0] + profile + contents[1]
-        # time.sleep(1) # sleeping to prevent throttling
-
-        print(element)
-
+    print("")
+    print("Modifying new control")
+    print("")
+    for i in range(len(account_ids)):
+        element = contents[0] + account_ids[i] + contents[1]
         try:
-            securityhub_client.update_standards_control(
+            securityhub_clients[i].update_standards_control(
                 StandardsControlArn=element,
                 ControlStatus="DISABLED",
                 DisabledReason="Not Applicable for this account"
@@ -61,36 +65,4 @@ for contents in file_contents:
                 f"Encountered parameter validation error: {e}"
             )
         except ClientError as e:
-            print("- already disabled")
-            # raise Exception(f"Failed to create security hub changes: {e}")
-
-
-# for single accounts, the following code should be used to prevent throttling
-# for profile in profile_list:
-#     print("")
-#     print("Suppressing rules for Account ID: " + profile)
-#     print("")
-#     session = boto3.Session(profile_name=profile)
-#     securityhub_client = session.client("securityhub", region_name="eu-west-2")
-
-#     for contents in file_contents:
-#         try:
-#             element = contents[0] + profile + contents[1]
-#             time.sleep(1) # sleeping to prevent throttling
-
-#             print(element)
-
-#             securityhub_client.update_standards_control(
-#                 StandardsControlArn=element,
-#                 ControlStatus="DISABLED",
-#                 DisabledReason="Not Applicable for this account"
-#             )
-#             print("Modified control " + element)
-#             logging.info(f"Modified SecurityHub Control")
-#         except ParamValidationError as e:
-#             raise Exception(
-#                 f"Encountered parameter validation error: {e}"
-#             )
-#         except ClientError as e:
-#             print("- already disabled")
-#             # raise Exception(f"Failed to create security hub changes: {e}")
+            print("- already disabled. Switching to next account")
