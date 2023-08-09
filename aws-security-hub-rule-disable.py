f71d612395667273a0a9001fb66a7fbbd8a844a4
security-man <zac.laker@crowncommercial.gov.uk>
2023-08-09 13:58:52 +0100

    Added README.md and modified code to make file reading simpler

--- aws-security-hub-rule-disable.py
+++ aws-security-hub-rule-disable.py
@@ -1,6 +1,7 @@
 import boto3
 import logging
 import csv
+import math
 from botocore.client import ClientError
 from botocore.exceptions import ParamValidationError
 
@@ -36,33 +37,41 @@ with open('control-suppression-list.csv') as f:
 # initiates sessions / clients per aws profile (account combined with security audit role)
 sessions = []
 securityhub_clients = []
-for account_id in account_ids:
-    profile = account_id + "-RO"
-    session = boto3.Session(profile_name=profile)
-    sessions.append(session)
-    securityhub_client = session.client("securityhub", region_name="eu-west-2")
-    securityhub_clients.append(securityhub_client)
+iterator = int(math.ceil(len(account_ids) / 10))
+for i in range(10):
+    if i < 9:
+        account_ids_segment = account_ids[i*iterator:i*iterator + iterator - 1]
+    else:
+        account_ids_segment = account_ids[i*iterator:len(account_ids) - 1]
+    for account_id in account_ids_segment:
+        profile = account_id + "-RO"
+        session = boto3.Session(profile_name=profile)
+        sessions.append(session)
+        securityhub_client = session.client("securityhub", region_name="eu-west-2")
+        securityhub_clients.append(securityhub_client)
+        print("Created new session and client")
 
-# loops through list of controls to suppress from csv, and loops through each individual profile to make the changes to
-# securityhub api is rate-limited to 1 request per second, per account
-for contents in file_contents:
-    print("")
-    print("Modifying new control")
-    print("")
-    for i in range(len(account_ids)):
-        element = contents[0] + account_ids[i] + contents[1]
-        try:
-            securityhub_clients[i].update_standards_control(
-                StandardsControlArn=element,
-                ControlStatus="DISABLED",
-                DisabledReason="Not Applicable for this account"
-            )
-            print("Modified control " + element)
-            logging.info(f"Modified SecurityHub Control")
-            
-        except ParamValidationError as e:
-            raise Exception(
-                f"Encountered parameter validation error: {e}"
-            )
-        except ClientError as e:
-            print("- already disabled. Switching to next account")
+    # loops through list of controls to suppress from csv, and loops through each individual profile to make the changes to
+    # securityhub api is rate-limited to 1 request per second, per account
+    for contents in file_contents:
+        print("")
+        print("Modifying new control " + contents[0])
+        print("")
+        for i in range(len(account_ids_segment)):
+            element = "arn:aws:securityhub:eu-west-2:" + account_ids_segment[i] + contents[0]
+            try:
+                print("Trying Account ID " + account_ids_segment[i])
+                securityhub_clients[i].update_standards_control(
+                    StandardsControlArn=element,
+                    ControlStatus="DISABLED",
+                    DisabledReason="Not Applicable for this account"
+                )
+                print("Modified control " + element)
+                logging.info(f"Modified SecurityHub Control")
+                
+            except ParamValidationError as e:
+                raise Exception(
+                    f"Encountered parameter validation error: {e}"
+                )
+            except ClientError as e:
+                print("- already disabled. Switching to next account")
