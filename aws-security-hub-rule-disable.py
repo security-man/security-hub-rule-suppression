c66f8e1d462fff807f76ee4d9ccca32f3839a6af
security-man <zac.laker@crowncommercial.gov.uk>
2024-04-05 14:11:28 +0100

    Modified code to execute using multiple threads. Hooray for parallelised code

--- aws-security-hub-rule-disable.py
+++ aws-security-hub-rule-disable.py
@@ -1,3 +1,4 @@
+import concurrent.futures
 import boto3
 import logging
 import csv
@@ -34,44 +35,35 @@ with open('control-suppression-list.csv') as f:
     for row in reader:
         file_contents.append(row)
 
-# initiates sessions / clients per aws profile (account combined with security audit role)
-sessions = []
-securityhub_clients = []
-iterator = int(math.ceil(len(account_ids) / 10))
-for i in range(10):
-    if i < 9:
-        account_ids_segment = account_ids[i*iterator:i*iterator + iterator - 1]
-    else:
-        account_ids_segment = account_ids[i*iterator:len(account_ids) - 1]
-    for account_id in account_ids_segment:
-        profile = account_id + "-RO"
-        session = boto3.Session(profile_name=profile)
-        sessions.append(session)
-        securityhub_client = session.client("securityhub", region_name="eu-west-2")
-        securityhub_clients.append(securityhub_client)
-        print("Created new session and client")
-
+def disable_rules_from_account(account_id):
+    # initiates sessions / clients per aws profile (account combined with security audit role)
+    profile = account_id + "-RO"
+    session = boto3.Session(profile_name=profile)
+    securityhub_client = session.client("securityhub", region_name="eu-west-2")
+    print("Created new session and client")
     # loops through list of controls to suppress from csv, and loops through each individual profile to make the changes to
     # securityhub api is rate-limited to 1 request per second, per account
     for contents in file_contents:
         print("")
         print("Modifying new control " + contents[0])
         print("")
-        for i in range(len(account_ids_segment)):
-            element = "arn:aws:securityhub:eu-west-2:" + account_ids_segment[i] + contents[0]
-            try:
-                print("Trying Account ID " + account_ids_segment[i])
-                securityhub_clients[i].update_standards_control(
-                    StandardsControlArn=element,
-                    ControlStatus="DISABLED",
-                    DisabledReason="Not Applicable for this account"
-                )
-                print("Modified control " + element)
-                logging.info(f"Modified SecurityHub Control")
+        element = "arn:aws:securityhub:eu-west-2:" + account_id + contents[0]
+        try:
+            print("Trying Account ID " + account_id)
+            securityhub_client.update_standards_control(
+                StandardsControlArn=element,
+                ControlStatus="DISABLED",
+                DisabledReason="Not Applicable for this account"
+            )
+            print("Modified control " + element)
+            logging.info(f"Modified SecurityHub Control")
                 
-            except ParamValidationError as e:
-                raise Exception(
-                    f"Encountered parameter validation error: {e}"
-                )
-            except ClientError as e:
-                print("- already disabled. Switching to next account")
+        except ParamValidationError as e:
+            raise Exception(
+                f"Encountered parameter validation error: {e}"
+            )
+        except ClientError as e:
+            print("- already disabled. Switching to next account")
+
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    executor.map(disable_rules_from_account,account_ids)
